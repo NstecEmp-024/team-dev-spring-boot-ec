a0a1ae15257715254c9e60b62f27a8b2780bde63
NstecEmp-019 <nstecemp_019@fullness.email>
2025-08-27 09:38:00 +0900

    NewItemHelperを追加

---
+++ src/main/java/com/fullness/ec/helper/NewItemHelper.java
@@ -0,0 +1,16 @@
+package com.fullness.ec.helper;
+
+import com.fullness.ec.entity.Product;
+import com.fullness.ec.form.NewItemForm;
+
+public class NewItemHelper {
+    public static Product convert(NewItemForm newItemForm) {
+        Product product = new Product();
+        product.setId(newItemForm.getId());
+        product.setCategoryId(newItemForm.getCategoryId());
+        product.setName(newItemForm.getName());
+        product.setPrice(newItemForm.getPrice());
+        product.setAmount(newItemForm.getAmount());
+        return product;
+    }
+}
