c6856bce481f34d516d713c09301f7a5d56671b7
NstecEmp-019 <nstecemp_019@fullness.email>
2025-08-27 09:43:04 +0900

    addProduct(NewItemForm newItemForm)とselectAll()を追加

--- src/main/java/com/fullness/ec/service/ProductService.java
+++ src/main/java/com/fullness/ec/service/ProductService.java
@@ -4,28 +4,34 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import com.fullness.ec.entity.Product;
+import com.fullness.ec.form.NewItemForm;
 
 /**
  * 商品に関連した内部処理インタフェース
  */
 public interface ProductService {
-    
-    /**
-     * 1ページ分の商品データを取得
-     * @param pageable ページ情報
-     * @return 商品データのページ
-     */
-    Page<Product> findProductByPage(Pageable pageable, Integer categoryId);
 
-    // /**
-    //  * 商品一覧を表示する
-    //  * @return 商品一覧
-    //  */
-    // List<Product> getAllProducts();
+  /**
+   * 1ページ分の商品データを取得
+   * 
+   * @param pageable ページ情報
+   * @return 商品データのページ
+   */
+  Page<Product> findProductByPage(Pageable pageable, Integer categoryId);
 
-     /**
-      * 商品検索（カテゴリ検索）
-      */
-    List<Product> findProductsByCategoryId(Integer categoryId);
+  // /**
+  // * 商品一覧を表示する
+  // * @return 商品一覧
+  // */
+  // List<Product> getAllProducts();
+
+  /**
+   * 商品検索（カテゴリ検索）
+   */
+  List<Product> findProductsByCategoryId(Integer categoryId);
+
+  void addProduct(NewItemForm newItemForm);
+
+  List<Product> selectAll();
 
 }
